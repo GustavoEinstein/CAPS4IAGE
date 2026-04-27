ad674531658967cefbcb1b7b8dcf68e0734560ae
GustavoEinstein <gustavoeinstein17@gmail.com>
2026-04-27 16:57:54 -0300

    ajuste de interceptação de url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,15 +6,18 @@ const api = axios.create({
 });
 
 // 1. INTERCEPTOR DE REQUISIÇÃO (Envia o Token automaticamente)
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('access_token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
+api.interceptors.response.use(
+    (response) => response,
     (error) => {
+        if (error.response && error.response.status === 401) {
+            // Se o erro vier de um arquivo de mídia, não desloga o usuário
+            if (error.config.url.includes('/media/')) {
+                return Promise.reject(error);
+            }
+            
+            localStorage.clear();
+            window.location.href = '/';
+        }
         return Promise.reject(error);
     }
 );
